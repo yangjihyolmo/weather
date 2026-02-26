ec6a14edf56a1f38e0dfe5678b093126ce8d38fd
Yangji <yangjuhyolmo13@gamil.com>
2026-02-26 10:25:53 +0545

    update everthing

--- index.js
+++ index.js
@@ -5,22 +5,18 @@ $(document).ready(function () {
         var city = $("#cityInput").val();
         var apiKey = "ab810afeab1fb1cbb8e5dd64e784ac62";
 
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=" 
-                    + city 
-                    + "&appid=" + apiKey 
-                    + "&units=metric";
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         $.get(url, function (data) {
             console.log(data)
 
             $("#city").text(data.name);
-            $("#temp").text(Math.round(data.main.temp) + "°C");
+            $("#temp").text(data.main.temp + "°C");
             $("#desc").text(data.weather[0].description);
 
-            // Weather Icon
             var iconCode = data.weather[0].icon;
-            var iconUrl = "https://openweathermap.org/img/wn/" 
-                            + iconCode + "@2x.png";
+            var iconUrl = "https://openweathermap.org/img/wn/"
+                + iconCode + "@2x.png";
 
             $("#icon").attr("src", iconUrl);
 
